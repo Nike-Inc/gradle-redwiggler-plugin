a51a770b4359da8dc081c76a1f16bd9cba089ecd
Tyler Southwick <tyler.southwick@nike.com>
2017-05-02 13:58:58 -0700

    add todo unit test

---
+++ src/test/groovy/com/nike/retail/common/plugins/gradle/redwiggler/RedWigglerPluginTests.groovy
@@ -0,0 +1,11 @@
+package com.nike.retail.common.plugins.gradle.redwiggler
+
+import org.junit.Test
+
+public class RedWigglerPluginTests {
+
+    @Test
+    public void todo() {
+        println("Todo validation")
+    }
+}
