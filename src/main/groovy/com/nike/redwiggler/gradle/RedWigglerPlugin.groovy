e2cb72a444374ee6e7b0508cb98f2828ab5aa656
Tyler Southwick <tyler.southwick@nike.com>
2017-09-24 07:22:37 -0700

    add unit test and allow setting of toolVersion

--- src/main/groovy/com/nike/redwiggler/gradle/RedWigglerPlugin.groovy
+++ src/main/groovy/com/nike/redwiggler/gradle/RedWigglerPlugin.groovy
@@ -1,9 +1,5 @@
 package com.nike.redwiggler.gradle
 
-import com.nike.redwiggler.core.GlobEndpointCallProvider
-import com.nike.redwiggler.core.Redwiggler
-import com.nike.redwiggler.html.HtmlReportProcessor
-import com.nike.redwiggler.swagger.SwaggerEndpointSpecificationProvider
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.slf4j.Logger
@@ -18,13 +14,29 @@ class RedWigglerPlugin implements Plugin<Project> {
         LOGGER.info('Running wiggler')
 
         project.extensions.create('redwiggler', RedWigglerPluginExtension, project)
-        project.task('runRedWigglerReport') << {
+        project.task('runRedWigglerReport') doLast {
             RedWigglerPluginExtension ext = project.redwiggler
-            Redwiggler.apply(
-                    new GlobEndpointCallProvider(ext.dataDirectory, ".*.json"),
-                    new SwaggerEndpointSpecificationProvider(ext.swaggerFile),
-                    new HtmlReportProcessor(ext.output)
+            project.configurations {
+                redwiggler
+            }
+            project.dependencies {
+                redwiggler group: 'com.nike.redwiggler', name: 'redwiggler-swagger_2.12', version: ext.toolVersion
+                redwiggler group: 'com.nike.redwiggler', name: 'redwiggler-reports-html_2.12', version: ext.toolVersion
+            }
 
+            def resolvedConfiguration = project.configurations.redwiggler.resolvedConfiguration
+            def files = resolvedConfiguration.files
+            def classpath = files.collect{it.toURI().toURL()}
+            def classloader = new URLClassLoader(classpath as URL[])
+
+            def redwiggler = classloader.loadClass("com.nike.redwiggler.core.Redwiggler")
+            def GlobEndpointCallProvider = classloader.loadClass("com.nike.redwiggler.core.GlobEndpointCallProvider")
+            def SwaggerEndpointSpecificationProvider = classloader.loadClass("com.nike.redwiggler.swagger.SwaggerEndpointSpecificationProvider")
+            def HtmlReportProcessor = classloader.loadClass("com.nike.redwiggler.html.HtmlReportProcessor")
+            redwiggler.apply(
+                    GlobEndpointCallProvider.newInstance(ext.dataDirectory, ".*.json"),
+                    SwaggerEndpointSpecificationProvider.apply(ext.swaggerFile),
+                    HtmlReportProcessor.newInstance(ext.output)
             )
         }
     }
@@ -34,10 +46,12 @@ class RedWigglerPluginExtension {
     File swaggerFile
     File dataDirectory
     File output
+    String toolVersion
 
     RedWigglerPluginExtension (Project project) {
         this.output = new File(project.getBuildDir(), "redwiggler.html")
-        this.dataDirectory = new File(new File(project.getBuildDir(), "reports"), "tests")
+        this.dataDirectory = new File(project.getBuildDir(), "redwiggler-data")
         this.swaggerFile = new File(project.rootDir, "swagger.yaml")
+        this.toolVersion = "0.5.1"
     }
 }
---
+++ src/test/groovy/com/nike/redwiggler/gradle/RedWigglerPluginSpec.groovy
@@ -0,0 +1,55 @@
+package com.nike.redwiggler.gradle
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import java.nio.file.Files
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class RedWigglerPluginSpec extends Specification {
+
+    @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+    def swaggerFile(String source) {
+        Files.copy(getClass().getResourceAsStream(source), new File(testProjectDir.root, "swagger.yaml").toPath())
+    }
+
+    def "sane defaults swith swagger.yaml"() {
+        given:
+        swaggerFile "simpleSwagger.yaml"
+        def requestsDir = testProjectDir.newFolder("build", "redwiggler-data")
+        def outputFile = new File(new File(testProjectDir.root, "build"), "redwiggler.html")
+        buildFile << """
+            plugins {
+                id 'com.nike.redwiggler'
+            }
+            repositories {
+                jcenter()
+            }
+        """
+
+        when:
+        def result = GradleRunner.create()
+                .withGradleVersion(gradleVersion)
+                .withProjectDir(testProjectDir.root)
+                .withArguments('runRedwigglerReport')
+                .withPluginClasspath()
+                .build()
+
+
+        then:
+        result.task(":runRedWigglerReport").outcome == SUCCESS
+        outputFile.exists()
+
+        where:
+        gradleVersion << ['4.0', '4.1', '4.2']
+    }
+}
